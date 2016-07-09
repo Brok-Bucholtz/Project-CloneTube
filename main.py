0292f3d34d672d3f85201321d7a5e374a1b2c4d3
code-rush <parikh.japan30@gmail.com>
2016-07-09 15:33:33 -0700

    Fetching Video Data API
    Api that gets data for a video

--- main.py
+++ main.py
@@ -14,21 +14,21 @@ def _mongo_to_json_response(mongo):
 
 @app.route('/')
 @app.route('/home')
-def WelcomPageAPI():
+def home_page():
    videos = mongo.db.videos.find()
    return render_template('home_page.html', videos=videos)
 
 
 @app.route('/home/video/<int:video_id>/')
-def ViewVideoPageAPI(video_id):
-   video = mongo.db.videos.find({id: video_id})
+def view_video_page(video_id):
+   video = mongo.db.videos.find({'id': video_id})
    return render_template('video_page.html', video=video)
 
 
 @app.route('/home/videos/most_recent/')
 @app.route('/home/videos/trending/')
-def GetNewestVideosAPI():
-   newest_videos = mongo.db.videos.find().sort({_id:-1}).limit(10)
+def get_newest_video():
+   newest_videos = mongo.db.videos.find().sort({'_id':-1}).limit(10)
    return render_template('recent_videos.html', newest_videos=newest_videos)
 
 
@@ -49,17 +49,10 @@ def post_video_comments(video_id):
     return _mongo_to_json_response(mongo.db.comments.insert(comment))
 
 
+@app.route('/home/videos/video_data/<int:video_id>')    
+def get_video_data(video_id):
+    return _mongo_to_json_response(mongo.db.videos.find({'video_id': video_id}))
 
-@app.route('/api/v1/videos/<int:video_id>/comments')
-def get_video_comments(video_id):
-    return _mongo_to_json_response(mongo.db.comments.find({'video_id': video_id}))
-
-
-@app.route('/api/v1/videos/<int:video_id>/comments', methods=['POST'])
-def post_video_comments(video_id):
-    comment = request.get_json()
-    comment['video_id'] = video_id
-    return _mongo_to_json_response(mongo.db.comments.insert(comment))
 
 
 if __name__ == '__main__':
