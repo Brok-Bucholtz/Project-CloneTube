f38acdb30907de80c6f9f335e2284c4077dbf56c
code-rush <parikh.japan30@gmail.com>
2016-07-08 23:37:16 -0700

    Initial Stage
    Application runs as expected.

--- main.py
+++ main.py
@@ -1,5 +1,14 @@
 from flask import Flask
-from flask.ext.pymongo import PyMongo
+from flask_pymongo import PyMongo
 
 app = Flask(__name__)
 mongo = PyMongo(app)
+
+@app.route("/")
+def hello():
+	return 'Hello World'
+
+if __name__ == "__main__":
+	app.secret_key = 'super_secret_key_that_no_one_can_break_in'
+	app.debug = True
+	app.run(host="0.0.0.0", port=7000)
