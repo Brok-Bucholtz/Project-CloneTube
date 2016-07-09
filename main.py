abd9720982b19467f8360cf00a4f9fe248f9980b
Brok Bucholtz <brok.bucholtz@gmail.com>
2016-07-09 16:27:26 -0700

    Fix indentation

--- main.py
+++ main.py
@@ -14,26 +14,21 @@ def _mongo_to_json_response(mongo):
 @app.route('/')
 @app.route('/home')
 def home_page():
-   videos = mongo.db.videos.find()
-   return render_template('home_page.html', videos=videos)
+    videos = mongo.db.videos.find()
+    return render_template('home_page.html', videos=videos)
 
 
 @app.route('/home/video/<int:video_id>/')
 def view_video_page(video_id):
-   video = mongo.db.videos.find({'video_id': video_id})
-   return render_template('video_page.html', video=video)
+    video = mongo.db.videos.find({'video_id': video_id})
+    return render_template('video_page.html', video=video)
 
 
 @app.route('/home/videos/most_recent/')
 @app.route('/home/videos/trending/')
 def get_newest_video():
-   newest_videos = mongo.db.videos.find().sort({'_id':-1}).limit(10)
-   return render_template('recent_videos.html', newest_videos=newest_videos)
-
-
-
-def _mongo_to_json_response(mongo):
-    return Response(json_util.dumps(mongo, indent=4, sort_keys=True), mimetype="application/json")
+    newest_videos = mongo.db.videos.find().sort({'_id':-1}).limit(10)
+    return render_template('recent_videos.html', newest_videos=newest_videos)
 
 
 @app.route('/api/v1/videos/<int:video_id>/comments')
