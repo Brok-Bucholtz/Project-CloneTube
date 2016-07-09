258683bf1cf06235a91927bafe3cc2046dd8f3b2
Brok Bucholtz <brok.bucholtz@gmail.com>
2016-07-09 12:23:26 -0700

    Fix formatting

--- main.py
+++ main.py
@@ -4,11 +4,12 @@ from flask_pymongo import PyMongo
 app = Flask(__name__)
 mongo = PyMongo(app)
 
-@app.route("/")
-def hello():
-	return 'Hello World'
 
-if __name__ == "__main__":
-	app.secret_key = 'super_secret_key_that_no_one_can_break_in'
-	app.debug = True
-	app.run(host="0.0.0.0", port=7000)
+@app.route('/')
+def hello():
+    return 'Hello World'
+
+if __name__ == '__main__':
+    app.secret_key = 'super_secret_key_that_no_one_can_break_in'
+    app.debug = True
+    app.run(host='0.0.0.0', port=7000)
