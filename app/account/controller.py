127d716bbbb7a854ea7cf5657769a93721847267
Brok Bucholtz <brok.bucholtz@gmail.com>
2016-07-10 11:16:38 -0700

    Move account assets to folder

--- app/account/controller.py
+++ app/account/controller.py
@@ -1,6 +1,6 @@
 from flask import Blueprint, render_template
 
-account_page_routes = Blueprint('account_page', __name__, template_folder='.')
+account_page_routes = Blueprint('account_page', __name__, template_folder='asset', static_folder='asset')
 
 
 @account_page_routes.route('/create')
