72f13238b46ed8e162e4b94f2b83540e0eb807ed
Japan Parikh <parikh.japan30@gmail.com>
2016-07-10 00:43:29 -0700

    new videos page controller

---
+++ app/video/newest_videos/controller.py
@@ -0,0 +1,8 @@
+from flask import Blueprint, render_template
+
+new_videos_routes = Blueprint('newest_videos', __name__, template_folder='.')
+
+@new_videos_routes.route("/newest_videos")
+def get_newest_video():
+    newest_videos = mongo.db.videos.find().sort({'_id':-1}).limit(10)
+    return render_template('newest_videos.html', newest_videos=newest_videos)
