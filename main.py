c1a75e5d29e3bec8e1b49b8a1029edc686afa195
Brok Bucholtz <brok.bucholtz@gmail.com>
2016-07-10 13:32:17 -0700

    Rename main file to run

--- main.py
+++
@@ -1,7 +0,0 @@
-from app.app import app
-
-
-if __name__ == '__main__':
-    app.secret_key = 'super_secret_key_that_no_one_can_break_in'
-    app.debug = True
-    app.run(host='0.0.0.0', port=7000)
