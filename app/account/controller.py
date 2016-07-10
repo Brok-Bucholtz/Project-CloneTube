234290d79cc4690474da48266c753e3f8da8753b
Brok Bucholtz <brok.bucholtz@gmail.com>
2016-07-10 00:11:29 -0700

    Rename account routes

--- app/account/controller.py
+++ app/account/controller.py
@@ -1,8 +1,8 @@
 from flask import Blueprint, render_template
 
-account_routes = Blueprint('account', __name__, template_folder='.')
+account_page_routes = Blueprint('account_page', __name__, template_folder='.')
 
 
-@account_routes.route('/create')
+@account_page_routes.route('/create')
 def create_account():
     return render_template('create.html')
